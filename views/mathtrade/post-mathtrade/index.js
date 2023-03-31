853ed97c13f3556c0a7950fee0b45911ff691e1b
Pablo Cazorla <miscuentas2pablo@gmail.com>
2023-03-31 13:47:20 -0300

    no results

--- views/mathtrade/post-mathtrade/index.js
+++ views/mathtrade/post-mathtrade/index.js
@@ -3,29 +3,45 @@ import PageHeader from "components/pageHeader";
 import MyItem from "./myItem";
 import I18N from "i18n";
 import LinkInternal from "components/link-internal";
+import ErrorAlert from "components/errorAlert";
 
-const PostMTView = ({ data, loading }) => {
+const PostMTView = ({ data, loading, errors }) => {
   return (
     <PrivateLayout loading={loading} doctitle="title.PostMT">
       <PageHeader title="title.PostMT" center />
-      <div className="post-mt-help">
-        <p className="lead">
-          <I18N id="postMT.help1a" />
-          <LinkInternal path={"results"} mathtrade>
-            <I18N id="postMT.results" />
-          </LinkInternal>
-          <I18N id="postMT.help1b" />
-        </p>
-        <p>
-          <I18N id="postMT.help2" />
-        </p>
-        <p>
-          <I18N id="postMT.help3" />
-        </p>
-      </div>
-      {data.map((item) => {
-        return <MyItem item={item} key={item.id} />;
-      })}
+      {data.length ? (
+        <>
+          <div className="post-mt-help">
+            <p className="lead">
+              <I18N id="postMT.help1a" />
+              <LinkInternal path={"results"} mathtrade>
+                <I18N id="postMT.results" />
+              </LinkInternal>
+              <I18N id="postMT.help1b" />
+            </p>
+            <p>
+              <I18N id="postMT.help2" />
+            </p>
+            <p>
+              <I18N id="postMT.help3" />
+            </p>
+          </div>
+          <ErrorAlert errors={errors} />
+          {data.map((item) => {
+            return <MyItem item={item} key={item.id} />;
+          })}
+        </>
+      ) : (
+        <div className="text-center">
+          <p className="">
+            <I18N id="postMT.Nothelp1a" />
+            <LinkInternal path={"results"} mathtrade>
+              <I18N id="postMT.results" />
+            </LinkInternal>
+            <I18N id="postMT.Nothelp1b" />
+          </p>
+        </div>
+      )}
     </PrivateLayout>
   );
 };
