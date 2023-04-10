1b238dfee3de4af494fb0947ea436a1074d487b0
Pablo Cazorla <miscuentas2pablo@gmail.com>
2023-04-10 16:19:46 -0300

    status desc

--- components/statusBadge/index.js
+++ components/statusBadge/index.js
@@ -1,12 +1,34 @@
-import { Badge } from "reactstrap";
+import {
+  Badge,
+  UncontrolledTooltip,
+  UncontrolledButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+} from "reactstrap";
 import { statusTypes } from "config";
+import { useId } from "react";
+import I18N from "i18n";
+
+const twoPointsReg = new RegExp(":", "g");
 
 const StatusBadge = ({ status, className }) => {
+  const id = useId("st-q").replace(twoPointsReg, "");
+
   return status && statusTypes[status] ? (
     <>
-      <Badge color={status.toLowerCase()} className={className}>
+      <Badge
+        color={status.toLowerCase()}
+        className={className}
+        id={`st-status-q-${id}`}
+      >
         {statusTypes[status]}
       </Badge>
+      <UncontrolledTooltip
+        //placement="right"
+        target={`st-status-q-${id}`}
+      >
+        <I18N id={`statusType.desc.${status}`} />
+      </UncontrolledTooltip>
     </>
   ) : null;
 };
