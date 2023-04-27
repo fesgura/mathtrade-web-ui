39a5ae1d1395d7444c064a97d138b905413de561
Pablo Cazorla <miscuentas2pablo@gmail.com>
2023-04-27 10:30:24 -0300

    lista de contactos

--- views/mathtrade/post-mathtrade/index.js
+++ views/mathtrade/post-mathtrade/index.js
@@ -1,28 +1,14 @@
 import { useEffect, useState } from "react";
-import { Row, Col } from "reactstrap";
 import PrivateLayout from "layouts/private";
 import PageHeader from "components/pageHeader";
-import MyItem from "./myItem";
 import I18N from "i18n";
 import LinkInternal from "components/link-internal";
-import ErrorAlert from "components/errorAlert";
-import Pagination from "components/pagination";
-import ElementPerPage from "components/pagination/elementsPerPage";
-import storage from "utils/storage";
+import PostMTList from "./list";
+import UserContacts from "./userContacts.js";
+import PageHeaderTabs from "components/pageHeaderTabs";
 
 const PostMTView = ({ data, loading, errors }) => {
-  const [page, set_page] = useState(0);
-  const [page_size, set_page_size] = useState(9999);
-
-  useEffect(() => {
-    const storeOptions = storage.getOptions();
-    if (storeOptions?.postmt_page) {
-      set_page(parseInt(storeOptions.postmt_page, 10));
-    }
-    if (storeOptions?.postmt_page_size) {
-      set_page_size(parseInt(storeOptions.postmt_page_size, 10));
-    }
-  }, []);
+  const [current, setCurrent] = useState(1);
 
   return (
     <PrivateLayout loading={loading} doctitle="title.PostMT">
@@ -44,95 +30,25 @@ const PostMTView = ({ data, loading, errors }) => {
               <I18N id="postMT.help3" values={["https://t.me/Luis_Olcese"]} />
             </p>
           </div>
-          <ErrorAlert errors={errors} />
-          <div className="pb-5">
-            <Row className="align-items-center justify-content-end">
-              <Col xs="auto">
-                <ElementPerPage
-                  min
-                  all
-                  filters={{
-                    query: {
-                      page_size,
-                      page: page + 1,
-                    },
-                  }}
-                  setFilters={(d) => {
-                    set_page(0);
-                    set_page_size(parseInt(d.page_size, 10));
-                    storage.setToOptions({
-                      postmt_page: 0,
-                      postmt_page_size: parseInt(d.page_size, 10),
-                    });
-                  }}
-                />
-              </Col>
-              <Col xs="auto">
-                <Pagination
-                  filters={{
-                    query: {
-                      page_size,
-                      page: page + 1,
-                    },
-                  }}
-                  setFilters={(d) => {
-                    set_page(parseInt(d.page, 10) - 1);
-                    storage.setToOptions({
-                      postmt_page: parseInt(d.page, 10) - 1,
-                    });
-                  }}
-                  elementsTotal={data.length || 1}
-                />
-              </Col>
-            </Row>
-          </div>
-          {data.map((item, k) => {
-            if (k < page * page_size || k >= (page + 1) * page_size) {
-              return null;
-            }
-            return <MyItem item={item} key={item.id} />;
-          })}
-          <div className="pt-3 pb-5">
-            <Row className="align-items-center justify-content-end">
-              <Col xs="auto">
-                <ElementPerPage
-                  min
-                  all
-                  filters={{
-                    query: {
-                      page_size,
-                      page: page + 1,
-                    },
-                  }}
-                  setFilters={(d) => {
-                    set_page(0);
-                    set_page_size(parseInt(d.page_size, 10));
-                    storage.setToOptions({
-                      postmt_page: 0,
-                      postmt_page_size: parseInt(d.page_size, 10),
-                    });
-                  }}
-                />
-              </Col>
-              <Col xs="auto">
-                <Pagination
-                  filters={{
-                    query: {
-                      page_size,
-                      page: page + 1,
-                    },
-                  }}
-                  setFilters={(d) => {
-                    set_page(parseInt(d.page, 10) - 1);
-                    storage.setToOptions({
-                      postmt_page: parseInt(d.page, 10) - 1,
-                    });
-                  }}
-                  elementsTotal={data.length || 1}
-                />
-              </Col>
-            </Row>
-          </div>
+          <PageHeaderTabs
+            className="mt-0 pt-0"
+            onChange={setCurrent}
+            tabs={[
+              {
+                text: "postMT.tab.1",
+                current: current === 0,
+              },
+              {
+                text: "postMT.tab.2",
+                current: current === 1,
+              },
+            ]}
+          />
+          {current === 0 ? (
+            <PostMTList data={data} errors={errors} />
+          ) : (
+            <UserContacts />
+          )}
         </>
       ) : (
         <div className="text-center">
