ef9d18492a7a4e6545e240b484eb2bd7f43111da
Pablo Cazorla <miscuentas2pablo@gmail.com>
2023-05-04 12:32:33 -0300

    added markdown

--- views/mathtrade/results/infoPayment/index.js
+++ views/mathtrade/results/infoPayment/index.js
@@ -1,9 +1,11 @@
 import { Card, CardBody } from "reactstrap";
+// import ReactMarkdown from 'react-markdown'
 
 const InfoPayment = () => {
   return <Card>
     <CardBody className="p-5 text-center">
       <p>Muy pronto vas a tener <b>AQUÍ</b><br/>la información de lo que <b>tenés que pagar de envío</b> por tus juegos.</p>
+      {/* <ReactMarkdown># Hello, *world*!</ReactMarkdown> */}
     </CardBody>
   </Card>
 }
