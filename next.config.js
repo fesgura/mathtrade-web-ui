6ace467b788276bfe358f36e995c6dc5e711d7e5
Pablo Cazorla <miscuentas2pablo@gmail.com>
2024-04-23 09:58:52 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     LINK_HELP_BGG:
       "https://boardgamegeek.com/thread/3007435/math-trade-argentina-abril-2023",
     LINK_HELP_TELEGRAM: "https://t.me/+vy8WiP3QbFtjNDhh",
-    LINK_HELP_VIDEO: "https://www.youtube.com/watch?v=U5cSBZeiNfc",
+    LINK_HELP_VIDEO: "https://www.youtube.com/watch?v=L1ri5Wz_HYw",
     LINK_HELP_ORGANIZATION: "https://t.me/Luis_Olcese",
   },
   reactStrictMode: false,
