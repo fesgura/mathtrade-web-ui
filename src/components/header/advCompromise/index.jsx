5519717739915ebfdc2ddd9f13f4bb18fb6cafd3
Pablo Cazorla <miscuentas2pablo@gmail.com>
2024-05-08 13:22:31 -0300

    fix shadow

--- src/components/header/advCompromise/index.jsx
+++ src/components/header/advCompromise/index.jsx
@@ -38,7 +38,7 @@ const AdvCompromise = () => {
   // end MY USER ********************************************
 
   return mustConfirm ? (
-    <div className="bg-red-600 absolute top-11 left-0 w-full z-[998] text-white text-center p-1">
+    <div className="bg-red-600 absolute top-11 left-0 w-full z-[998] text-white text-center p-1 shadow-md">
       <I18N id="AdvCompromise" />
       <Link
         href={PRIVATE_ROUTES.WANTS.path}
