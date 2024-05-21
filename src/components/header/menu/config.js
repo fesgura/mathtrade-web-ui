105fbb3347084157abc3c4a6e99d0f704ea84fa5
Pablo Cazorla <miscuentas2pablo@gmail.com>
2024-05-21 16:03:44 -0300

    reponer menu

--- src/components/header/menu/config.js
+++ src/components/header/menu/config.js
@@ -5,19 +5,19 @@ export const MenuList = [
     title: `menu.${PRIVATE_ROUTES.MY_COLLECTION.title}`,
     path: PRIVATE_ROUTES.MY_COLLECTION.path,
     name: "myCollection",
-    disabled: true,
+    //disabled: true,
   },
   {
     title: `menu.${PRIVATE_ROUTES.MY_OFFER.title}`,
     path: PRIVATE_ROUTES.MY_OFFER.path,
     name: "myOffer",
-    disabled: true,
+    //disabled: true,
   },
   {
     title: `menu.OfferGames`,
     path: PRIVATE_ROUTES.OFFER.path,
     name: "offer",
-    disabled: true,
+    //disabled: true,
     /*  subMenu: [
       {
         title: `menu.${PRIVATE_ROUTES.GAMES.title}`,
@@ -35,7 +35,7 @@ export const MenuList = [
     title: `menu.${PRIVATE_ROUTES.WANTS.title}`,
     path: PRIVATE_ROUTES.WANTS.path,
     name: "myWants",
-    disabled: true,
+    // disabled: true,
   },
   {
     title: `menu.${PRIVATE_ROUTES.RESULTS.title}`,
@@ -46,13 +46,13 @@ export const MenuList = [
     title: `menu.${PRIVATE_ROUTES.STATS.title}`,
     path: PRIVATE_ROUTES.STATS.path,
     name: "stats",
-    disabled: true,
+    //disabled: true,
   },
   {
     title: `menu.${PRIVATE_ROUTES.MY_DATA.title}`,
     path: PRIVATE_ROUTES.MY_DATA.path,
     name: "myData",
-    disabled: true,
+    //disabled: true,
   },
 ];
 
