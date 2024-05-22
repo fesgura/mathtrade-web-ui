b5de2b4d5b21c085ec2764576d2d53552e0a05a4
Pablo Cazorla <miscuentas2pablo@gmail.com>
2024-05-22 20:01:23 -0300

    Update index.jsx

--- src/app/mathtrade/homeContent/votacion/index.jsx
+++ src/app/mathtrade/homeContent/votacion/index.jsx
@@ -2,12 +2,13 @@ import Container from "@/components/container";
 import VotacionForm from "./form";
 import Results from "./results";
 
+const showResultes = false;
+
 const Votacion = () => {
   return (
     <Container>
       <div className="bg-white px-5 py-4 rounded-xl shadow-xl max-w-4xl mx-auto ">
-        {/* <VotacionForm /> */}
-        <Results />
+        {showResultes ? <Results /> : <VotacionForm />}
       </div>
     </Container>
   );
