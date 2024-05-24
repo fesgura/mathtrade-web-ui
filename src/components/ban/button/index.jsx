4fe74ee385f96fbf29b5faac3534d9edde1659e8
Pablo Cazorla <miscuentas2pablo@gmail.com>
2024-05-24 11:30:34 -0300

    Update index.jsx

--- src/components/ban/button/index.jsx
+++ src/components/ban/button/index.jsx
@@ -1,14 +1,15 @@
-import { useContext } from "react";
+/* import { useContext } from "react";
 import { ItemContext } from "@/context/item";
-import BanButtonUI from "./ui";
+import BanButtonUI from "./ui"; */
 
 const BanButton = ({ className, type = "item", size = "xl" }) => {
+  return null;
   /* ITEM CONTEXT **********************************************/
-  const { item } = useContext(ItemContext);
+ // const { item } = useContext(ItemContext);
   /* end ITEM CONTEXT */
 
-  return item?.isOwned ? null : (
+  /* return item?.isOwned ? null : (
     <BanButtonUI className={className} type={type} size={size} />
-  );
+  ); */
 };
 export default BanButton;
