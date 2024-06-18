70e533a6b7eb6770818f5a72890554c4a8842d7c
Pablo Cazorla <miscuentas2pablo@gmail.com>
2024-06-18 17:36:28 -0300

    Update page.jsx

--- src/app/sign/in/page.jsx
+++ src/app/sign/in/page.jsx
@@ -14,7 +14,7 @@ import { Suspense } from "react";
 
 const pausedSite = process.env.PAUSED_SITE;
 
-const SignInPage = () => {
+const SignInContent = () => {
   const {
     validations,
     onSubmit,
@@ -26,88 +26,94 @@ const SignInPage = () => {
   } = useSignIn();
 
   return (
-    <Suspense>
-      <div>
-        {changePasswordRequiredToken ? (
-          <ChangePassword
-            setChangePasswordRequiredToken={setChangePasswordRequiredToken}
-            token={changePasswordRequiredToken}
-            old_password={old_password}
-          />
-        ) : (
-          <>
-            <div className="text-center pb-3">
-              <h1 className="text-3xl  font-bold mb-3">
-                <I18N id="sign.Title" />
-              </h1>
-              <p className="text-gray-400">
-                <I18N id="sign.instruction" />
-              </p>
+    <div>
+      {changePasswordRequiredToken ? (
+        <ChangePassword
+          setChangePasswordRequiredToken={setChangePasswordRequiredToken}
+          token={changePasswordRequiredToken}
+          old_password={old_password}
+        />
+      ) : (
+        <>
+          <div className="text-center pb-3">
+            <h1 className="text-3xl  font-bold mb-3">
+              <I18N id="sign.Title" />
+            </h1>
+            <p className="text-gray-400">
+              <I18N id="sign.instruction" />
+            </p>
+          </div>
+          <Form validations={validations} onSubmit={onSubmit}>
+            <fieldset>
+              <InputContainer validate="email">
+                <Label text="form.Email" name="email" required />
+                <Input
+                  name="email"
+                  type="email"
+                  placeholder="form.Email.placeholder"
+                  autocomplete="username"
+                  ariaLabel="form.Email"
+                  icon="email"
+                />
+              </InputContainer>
+              <InputContainer validate="password" className="mb-0">
+                <Label text="form.Password" name="password" required />
+                <Input
+                  name="password"
+                  type="password"
+                  placeholder="********"
+                  noTranslatePlaceholder
+                  autocomplete="password"
+                  ariaLabel="form.Password"
+                  icon="password"
+                />
+              </InputContainer>
+            </fieldset>
+            <div className="text-center pt-1 mb-6 text-xs">
+              <Alink href={PUBLIC_ROUTES.FORGOT_PASSWORD.path}>
+                <I18N id="sign.ForgotPassword" />
+              </Alink>
             </div>
-            <Form validations={validations} onSubmit={onSubmit}>
-              <fieldset>
-                <InputContainer validate="email">
-                  <Label text="form.Email" name="email" required />
-                  <Input
-                    name="email"
-                    type="email"
-                    placeholder="form.Email.placeholder"
-                    autocomplete="username"
-                    ariaLabel="form.Email"
-                    icon="email"
-                  />
-                </InputContainer>
-                <InputContainer validate="password" className="mb-0">
-                  <Label text="form.Password" name="password" required />
-                  <Input
-                    name="password"
-                    type="password"
-                    placeholder="********"
-                    noTranslatePlaceholder
-                    autocomplete="password"
-                    ariaLabel="form.Password"
-                    icon="password"
-                  />
-                </InputContainer>
-              </fieldset>
-              <div className="text-center pt-1 mb-6 text-xs">
-                <Alink href={PUBLIC_ROUTES.FORGOT_PASSWORD.path}>
-                  <I18N id="sign.ForgotPassword" />
-                </Alink>
-              </div>
-              <ErrorAlert error={error} />
+            <ErrorAlert error={error} />
 
-              {pausedSite === "yes" ? (
-                <div className="text-center text-teal-600 font-bold mb-5 border border-teal-600 rounded-lg p-3">
-                  <I18N id="paused.site" />
-                </div>
-              ) : null}
-              <div className="text-center mb-5">
-                <Button ariaLabel="btn.Enter" disabled={pausedSite === "yes"}>
-                  <I18N id="btn.Enter" />
-                </Button>
+            {pausedSite === "yes" ? (
+              <div className="text-center text-teal-600 font-bold mb-5 border border-teal-600 rounded-lg p-3">
+                <I18N id="paused.site" />
               </div>
-            </Form>
-            <hr className="my-5" />
-            <div className="text-center">
-              <p className="text-gray-500 text-xs mb-4">
-                <I18N id="sign.IfDontCreateAccount" />
-              </p>
-              <Button
-                tag="link"
-                outline
-                sm
-                color="secondary"
-                href={PUBLIC_ROUTES.REGISTER.path}
-                ariaLabel="btn.Register"
-              >
-                <I18N id="btn.Register" />
+            ) : null}
+            <div className="text-center mb-5">
+              <Button ariaLabel="btn.Enter" disabled={pausedSite === "yes"}>
+                <I18N id="btn.Enter" />
               </Button>
             </div>
-            <LoadingBox loading={loading} />
-          </>
-        )}
-      </div>
+          </Form>
+          <hr className="my-5" />
+          <div className="text-center">
+            <p className="text-gray-500 text-xs mb-4">
+              <I18N id="sign.IfDontCreateAccount" />
+            </p>
+            <Button
+              tag="link"
+              outline
+              sm
+              color="secondary"
+              href={PUBLIC_ROUTES.REGISTER.path}
+              ariaLabel="btn.Register"
+            >
+              <I18N id="btn.Register" />
+            </Button>
+          </div>
+          <LoadingBox loading={loading} />
+        </>
+      )}
+    </div>
+  );
+};
+
+const SignInPage = () => {
+  return (
+    <Suspense>
+      <SignInContent />
     </Suspense>
   );
 };
