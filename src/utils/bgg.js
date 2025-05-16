2f3f5d194fa83cbedcbfc310cc2b87f62459133c
Pablo Cazorla <miscuentas2pablo@gmail.com>
2025-05-16 16:00:22 -0300

    add contain_ids in element editor

--- src/utils/bgg.js
+++ src/utils/bgg.js
@@ -17,6 +17,7 @@ export const extractBGGdataFromElement = (data) => {
     poll,
     statistics,
     yearpublished,
+    link,
   } = bggElement;
 
   // NAMES *******************************************************
@@ -45,6 +46,22 @@ export const extractBGGdataFromElement = (data) => {
     boardgame: 1,
     boardgameexpansion: 2,
   };
+
+  // CONTAINS *******************************************************
+  const contain_ids = (() => {
+    // const list = [`${bgg_id}`]
+
+    if (!link || link.length === 0) {
+      return `${bgg_id}`;
+    }
+
+    const list = link
+      .filter(({ type }) => type === "boardgamecompilation")
+      .map(({ id }) => id);
+
+    return [`${bgg_id}`].concat(list).join(",");
+  })();
+  // end CONTAINS *******************************************************
 
   // POLL *******************************************************
   // Dependency
@@ -254,6 +271,7 @@ export const extractBGGdataFromElement = (data) => {
       ...dependencyPoll,
       ...stats,
       ...players,
+      contain_ids,
     },
     versions,
   };
