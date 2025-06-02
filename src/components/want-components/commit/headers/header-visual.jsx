5d91871123bca49ea2c30be43d583b0a99ce4813
Pablo Cazorla <miscuentas2pablo@gmail.com>
2025-06-02 16:25:15 -0300

    Update header-visual.jsx

--- src/components/want-components/commit/headers/header-visual.jsx
+++ src/components/want-components/commit/headers/header-visual.jsx
@@ -2,12 +2,17 @@ import { PageContext } from "@/context/page";
 import { useContext, lazy } from "react";
 import I18N from "@/i18n";
 import Dynamic from "@/components/dynamic";
+import { useOptions } from "@/store";
 
 const EarlyPayMessage = lazy(() => import("./earlyPayMessage"));
 
 const CommitHeaderVisual = () => {
   const { mustConfirm, mustConfirmDate, canI, isUserEarlyPay } =
     useContext(PageContext);
+
+  /* FILTER OPTIONS **********************************************/
+  const filters_wants = useOptions((state) => state.filters_wants);
+  /* end FILTER OPTIONS *********************************************/
 
   if (mustConfirm && !canI.commit) {
     return (
@@ -27,6 +32,13 @@ const CommitHeaderVisual = () => {
         <div className="max-w-3xl mx-auto text-center text-balance pb-5 font-bold text-danger">
           <I18N id="CommitHeaderVisual.text" />
         </div>
+        {filters_wants.keyword ? (
+          <div className="border-b-2 border-gray-300 pb-10">
+            <div className="max-w-3xl mx-auto text-center text-balance font-bold">
+              <I18N id="CommitFooterVisual.clearFilter" />
+            </div>
+          </div>
+        ) : null}
       </>
     );
   }
