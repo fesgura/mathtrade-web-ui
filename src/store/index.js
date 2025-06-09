745df6487871d58834b0f26489629a1296261758
Pablo Cazorla <miscuentas2pablo@gmail.com>
2025-06-09 14:25:13 -0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -12,8 +12,8 @@ const STORE_DEFAULT = {
   locations: null,
 };
 
-const STORAGE_NAME = "MT_ARG_STORE_MAYO_25_hb";
-const OPTIONS_NAME = "MT_ARG_OPTIONS_MAYO_25_hb";
+const STORAGE_NAME = "MT_ARG_STORE_MAYO_25_hc";
+const OPTIONS_NAME = "MT_ARG_OPTIONS_MAYO_25_hc";
 
 export const useOptions = create(
   persist(
