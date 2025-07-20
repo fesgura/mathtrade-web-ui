750b4bc63104a6010c512fb851e05c03d10cd7c3
Florencia Lopez Segura <florencia.lopez@pedidosya.com>
2025-07-20 05:10:36 -0300

    config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  
   env: {
     PAUSED_SITE: "no",
     //
     API_TEST_MODE: "no",
     BASE_URL_TEST: "https://mathtrade-backend.fly.dev/",
-    BASE_URL: "https://api.mathtrade.com.ar/",
     //
-    GOOGLE_RECAPTCHA_CLIENT_KEY: "6LeWcz8gAAAAAGgpOiINIJZSwsmKH-eMjtbQbFbF",
+    // GOOGLE_RECAPTCHA_CLIENT_KEY: "6LeWcz8gAAAAAGgpOiINIJZSwsmKH-eMjtbQbFbF",
     //
     LINK_HELP_BGG:
       "https://boardgamegeek.com/thread/3007435/math-trade-argentina-abril-2023",
